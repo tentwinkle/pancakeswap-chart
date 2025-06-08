102bb2902746d1bf239a0d498181702b372616cb
tentwinkle <lucas.silins.me@gmail.com>
2025-06-08 09:23:17 -0700

    fix: resolve dependency errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'pancakeswap-candlestick',
+  description: 'PancakeSwap Candlestick App',
+  generator: 'Lukas',
 }
 
 export default function RootLayout({
